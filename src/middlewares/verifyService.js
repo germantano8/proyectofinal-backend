b9ba626638fd9016b55e4f0cd260e859b2f17093
Angelina Bazzi <angibazzi@gmail.com>
2023-09-28 15:54:14 -0300

    modified:   src/middlewares/verifyService.js 	modified:   src/models/service.js 	modified:   src/routes/index.js

--- src/middlewares/verifyService.js
+++ src/middlewares/verifyService.js
@@ -18,6 +18,8 @@ const verifyService = async (req, res, next) => {
                 return res.status(404).json({message: 'Service no encontrado'});
             }
         }
+        
+        errors.fecha_nacimiento = validator.isDate(req.body.fecha_nacimiento, { format: 'YYYY-MM-DD' }) ? null : "La fecha de nacimiento debe ser de formato: aaaa-mm-dd";
 
         errors.descripcion = validator.isAlpha(req.body.descripcion) && validator.isLength(req.body.descripcion, {min: 3, max: 45}) ? null : "La descripcion solo puede contener letras y debe tener entre 3 y 45 caracteres";
         errors.tipo_licencia = validator.isLength(req.body.tipo_licencia, {min: 1, max: 10}) && validator.isAlphanumeric(req.body.tipo_licencia) ? null : "El tipo de licencia debe ser alfanumérico y debe tener entre 1 y 10 caracteres";
