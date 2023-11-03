d52ea3d7b60944da751636a3805852b2fd6c80b5
Germán Tano <germantano88@gmail.com>
2023-11-03 18:26:32 -0300

    eliminado cookie-parser

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 require('dotenv').config();
 require('./src/database/connection');
 
@@ -14,7 +13,6 @@ module.exports = class App{
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(cors());
-        this.app.use(cookieParser());
         this.app.use('/api', routes);
     }
     
